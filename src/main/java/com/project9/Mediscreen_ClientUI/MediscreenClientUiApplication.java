348f54d8a8f4aa5456fb65beafafac65711fe674
Hendrick Osseux <osseux.hendrick@gmail.com>
2023-01-09 12:49:28 +0100

    First commit

---
+++ src/main/java/com/project9/Mediscreen_ClientUI/MediscreenClientUiApplication.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_ClientUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenClientUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenClientUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project9/Mediscreen_ClientUI/MediscreenClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_ClientUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenClientUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
