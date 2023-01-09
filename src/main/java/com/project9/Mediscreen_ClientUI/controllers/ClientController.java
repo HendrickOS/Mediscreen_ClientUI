4406040607059b2ca3034e4beab25a02d0fd7d24
Hendrick Osseux <osseux.hendrick@gmail.com>
2023-01-09 16:33:20 +0100

    Add MicroservicePatientsProxy & Update ClientController

--- src/main/java/com/project9/Mediscreen_ClientUI/controllers/ClientController.java
+++ src/main/java/com/project9/Mediscreen_ClientUI/controllers/ClientController.java
@@ -1,16 +1,27 @@
 package com.project9.Mediscreen_ClientUI.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.project9.Mediscreen_ClientUI.beans.PatientBean;
+import com.project9.Mediscreen_ClientUI.proxies.MicroservicePatientsProxy;
+
 @Controller
 public class ClientController {
 
+	private final MicroservicePatientsProxy patientsProxy;
+
+	public ClientController(MicroservicePatientsProxy patientsProxy) {
+		this.patientsProxy = patientsProxy;
+	}
+
 	@RequestMapping("/")
-
 	public String accueil(Model model) {
-
+		List<PatientBean> patients = patientsProxy.patientsList();
+		model.addAttribute("patients", patients);
 		return "Accueil";
 
 	}
--- src/main/java/com/project9/Mediscreen_ClientUI/proxies/MicroservicePatientsProxy.java
+++ src/main/java/com/project9/Mediscreen_ClientUI/proxies/MicroservicePatientsProxy.java
@@ -1,5 +1,16 @@
 package com.project9.Mediscreen_ClientUI.proxies;
 
-public class MicroservicePatientsProxy {
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project9.Mediscreen_ClientUI.beans.PatientBean;
+
+@FeignClient(name = "patient_microservice", url = "localhost:8081")
+public interface MicroservicePatientsProxy {
+
+	@GetMapping(value = "/patients/list")
+	List<PatientBean> patientsList();
 
 }
