1f77839153ba6be08e3ac7cbd2e36b8414786095
Hendrick Osseux <osseux.hendrick@gmail.com>
2023-01-09 23:22:26 +0100

    Edit MicroservicePatientsProxy

--- src/main/java/com/project9/Mediscreen_ClientUI/proxies/MicroservicePatientsProxy.java
+++ src/main/java/com/project9/Mediscreen_ClientUI/proxies/MicroservicePatientsProxy.java
@@ -3,14 +3,15 @@ package com.project9.Mediscreen_ClientUI.proxies;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project9.Mediscreen_ClientUI.beans.PatientBean;
 
-@FeignClient(name = "patient_microservice", url = "localhost:8081")
+@FeignClient(name = "microservice-patient", url = "localhost:8081")
 public interface MicroservicePatientsProxy {
 
-	@GetMapping(value = "/patients/list")
+//	@GetMapping(value = "/patients/list")
+	@RequestMapping(value = "/patients/list")
 	List<PatientBean> patientsList();
 
 }
